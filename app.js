e2d0a1fb495156efcbbb0ccd723b9b6d7af51dca
unknown <archernich@gmail.com>
2021-03-28 19:37:14 -0700

    modularize files

--- app.js
+++ app.js
@@ -134,15 +134,44 @@ const promptProject = (portfolioData) => {
     });
 };
     
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         const pageHTML = generatePage(portfolioData);
+
+
+//         fs.writeFile('./dist/index.html', pageHTML, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             };
+//             console.log('Portfolio complete! Check out index.html to see the output!');
+
+//             fs.copyFile('./src/style.css', './dist/style.css', err => {
+//                 if (err) {
+//                     console.log(err);
+//                     return;
+//                 }
+//                 console.log('Style sheet copied successfully!');
+//             });
+//         });
+//     });
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-            console.log('Portfolio complete! Check out index.html to see the output!');
-        });
-
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return msWriteProfilerMark(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
